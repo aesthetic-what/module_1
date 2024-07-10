60c37d0931c4d2b1bcd719f53b4034cee1917bba
aesthetic-what <ladnoladno477@gmail.com>
2024-07-10 12:38:19 +0500

    homework3.py

--- homework3.py
+++ homework3.py
@@ -2,7 +2,6 @@ name = "Тимур"
 print("Имя:",name)
 age = 18
 print("Возраст:",age)
-newAge = age + 1
-print("Новый возраст:",newAge)
+print("Новый возраст:",age + 1)
 is_Student = True
 print("Студент:",is_Student)
